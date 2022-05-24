21d3af3a36e36774473394dac804aaba3acd838d
Denise Sille <silleferrarid@gmail.com>
2022-05-23 23:52:15 -0300

    arrays

---
+++ js/destinos.js
@@ -0,0 +1,13 @@
+/**var destinos = ["Peru, Bolivia, Ecuador"];
+destinos.push("Uruguay");
+destinos.unshift("Chile");
+console.log (destinos);/** */
+
+var obj = null,
+    data = ['platano', 'manzana', 'papaya'];
+
+for(var i = 0; i <= data.length -1; i++) {
+    if(data[i] === 'platano') {
+        obj = data[i];
+    }
+}
